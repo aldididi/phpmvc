0460b4de20af21b6a5ea971da524e2ac19f153d4
aldididi <aldyh45@gmail.com>
2020-10-21 16:49:15 +0700

    still error  Trying to access array offset on value of type null

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,50 +1,24 @@
  <?php
 
  class Mahasiswa_model{
-    /* private $mhs = [
-        [
-            "nama" => "aldy",
-            "nim" => "1234",
-            "jurusan" => "Matematika"
-        ],
-        [
-            "nama" => "didi",
-            "nim" => "345",
-            "jurusan" => "Fisika"
-        ],
-        [
-            "nama" => "Shazqia",
-            "nim" => "4567",
-            "jurusan" => "Elektro"
-        ],
-        [
-            "nama" => "Syafina",
-            "nim" => "5876",
-            "jurusan" => "Bispro"
-        ],
-        [
-            "nama" => "Suci",
-            "nim" => "328654",
-            "jurusan" => "Akuntansi"
-        ],
 
-    ]; */
-    private $dbh; //database handler
-    private $stmt; //statment
+    private $table = 'mahasiswa';
+    private $db;
 
-    public function __construct(){
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=phpmvc';
-        try{
-         $this->dbh = new PDO($dsn, 'root', '');   
-        } catch(PDOException $e){
-            die($e -> getMessage());
-        }
+    public function __construct()
+    {
+        $this->db = new Database;
     }
 
-     public function getAllMahasiswa(){
-         $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-         $this->stmt->execute();
-         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    public function getAllMahasiswa(){
+         $this->db->query('SELECT * FROM ' . $this->table);
+         return $this->db->resultSet();
+        
+     }
+
+     public function getMahasiswaById($id){
+         $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=id ');
+         $this->db->bind('id', $id);
+         return $this->db->single();
      }
  }
