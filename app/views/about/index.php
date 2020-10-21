874f92aeb01beaec91a871d85e4f02bc7c0b7d86
aldididi <aldyh45@gmail.com>
2020-10-21 11:31:58 +0700

    adding assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>about<h1>
+<div class="container">
+    <img src="<?= BASEURL; ?>/img/index.jpg" alt="codingan" width="200" class="rounded-circle">
+    <h1 class="mt=4">about<h1>
     <p>Nama saya adalah <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?>, saya adalah <?= $data['pekerjaan'] ?></p>
+</div>
